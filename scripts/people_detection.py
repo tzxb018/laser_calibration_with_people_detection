7f5c62c5e24bf26a798cf5974a8c17ffe021800c
tzxb018 <tomobessho018@gmail.com>
2019-08-01 11:48:58 -0700

    looks at 5 frames in past and uses variance to find movement

--- scripts/people_detection.py
+++ scripts/people_detection.py
@@ -22,10 +22,10 @@ pc_li_mouse = PointCloud()
 pc_display = PointCloud()
 laser_pub = []
 laser_in = LaserScan()
-old_data_hog = []
-old_data_snake = []
-old_data_mouse = []
-change_in_time = .02
+old_data_hog1, old_data_hog2, old_data_hog3, old_data_hog4 = [], [], [], []
+old_data_snake1, old_data_snake2, old_data_snake3, old_data_snake4 = [], [], [], []
+old_data_mouse1, old_data_mouse2, old_data_mouse3, old_data_mouse4 = [], [], [], []
+change_in_time = .05
 max_range = 25.0
 pc_li_hog_all = PointCloud()
 pc_li_snake_all = PointCloud()
@@ -34,81 +34,211 @@ pc_display_all = PointCloud()
 
 
 def callback_hog_laser_init(data):
-    global pc_li_hog, old_data_hog
+    global pc_li_hog
     pc_li_hog = make_PC_from_Laser_display(data)
 
 def callback_hog_laser(data):
-    global pc_li_hog, old_data_hog, pc_li_hog_all
+    global pc_li_hog, old_data_hog1, old_data_hog2, old_data_hog3, old_data_hog4, pc_li_hog_all
 
+    # have 5 arrays store 5 most recent laser scans
+    # find variance of each index in 5 frames
+    # if variance is greater than threshold, then detect as movmenet, else nah
     points_to_be_converted = []
     print("hog")
 
-    if old_data_hog:
+    # if there is history of the laser scans (should work besides the very first laser scan)
+    if old_data_hog1 and old_data_hog2 and old_data_hog3 and old_data_hog4:
+
+        # convert to lists
         data.ranges = list(data.ranges)
+        old_data_hog1.ranges = list(old_data_hog1.ranges)
+        old_data_hog2.ranges = list(old_data_hog2.ranges)
+        old_data_hog3.ranges = list(old_data_hog3.ranges)
+        old_data_hog4.ranges = list(old_data_hog4.ranges)
+
+        # iterating through each angle of the laser
         for i in range(0, len(data.ranges)):
-            if abs(data.ranges[i] - old_data_hog.ranges[i]) >= change_in_time:
+            # if abs(data.ranges[i] - old_data_hog1.ranges[i]) >= change_in_time:
+            #     points_to_be_converted.append(data.ranges[i])
+            # else:
+            #     points_to_be_converted.append(numpy.inf)
+
+            # calculate the variance of the points of the 5 frames at each angle
+            variance_between_points = numpy.var([old_data_hog1.ranges[i], old_data_hog2.ranges[i], old_data_hog3.ranges[i],
+                          old_data_hog4.ranges[i], data.ranges[i]])
+            print(variance_between_points)
+
+            # if the variance exists and is greater than a threshold, add the point to another array
+            # this determines if the point is part of a moving object or not
+            if not math.isnan(variance_between_points) and variance_between_points >= change_in_time:
                 points_to_be_converted.append(data.ranges[i])
             else:
+
+                # append inf to make sure the point cloud does not display this angle
                 points_to_be_converted.append(numpy.inf)
     else:
         print("all new")
+
+        # add old data to the arrays as a placeholder
+        old_data_hog1 = data
+        old_data_hog2 = data
+        old_data_hog3 = data
+        old_data_hog4 = data
         points_to_be_converted = data.ranges
 
-    old_data_hog = data
+    # slide the histories of the laser scan one more array down (keeps the 5 most recent laser scans)
+    old_data_hog4 = old_data_hog3
+    old_data_hog3 = old_data_hog2
+    old_data_hog2 = old_data_hog1
+    old_data_hog1 = data
+
+    # init another laser scan just for the points that are tracked as movement
     change_hog = LaserScan()
     change_hog = copy.deepcopy(data)
     change_hog.ranges = points_to_be_converted
+    # print(len(points_to_be_converted))
+
+    # make point clouds for the movement
     pc_li_hog = make_PC_from_Laser_display(change_hog)
+
+    # make point clouds for the whole laser scan
     pc_li_hog_all = make_PC_from_Laser_display(data)
 
 def callback_mouse_laser(data):
-    global pc_li_mouse, old_data_mouse, pc_li_mouse_all
+    global pc_li_mouse, old_data_mouse1, old_data_mouse2, old_data_mouse3, old_data_mouse4, pc_li_mouse_all
 
+    # have 5 arrays store 5 most recent laser scans
+    # find variance of each index in 5 frames
+    # if variance is greater than threshold, then detect as movmenet, else nah
     points_to_be_converted = []
     print("mouse")
 
-    if old_data_mouse:
+    # if there is history of the laser scans (should work besides the very first laser scan)
+    if old_data_mouse1 and old_data_mouse2 and old_data_mouse3 and old_data_mouse4:
+
+        # convert to lists
         data.ranges = list(data.ranges)
+        old_data_mouse1.ranges = list(old_data_mouse1.ranges)
+        old_data_mouse2.ranges = list(old_data_mouse2.ranges)
+        old_data_mouse3.ranges = list(old_data_mouse3.ranges)
+        old_data_mouse4.ranges = list(old_data_mouse4.ranges)
+
+        # iterating through each angle of the laser
         for i in range(0, len(data.ranges)):
-            if abs(data.ranges[i] - old_data_mouse.ranges[i]) >= change_in_time:
+            # if abs(data.ranges[i] - old_data_hog1.ranges[i]) >= change_in_time:
+            #     points_to_be_converted.append(data.ranges[i])
+            # else:
+            #     points_to_be_converted.append(numpy.inf)
+
+            # calculate the variance of the points of the 5 frames at each angle
+            variance_between_points = numpy.var(
+                [old_data_mouse1.ranges[i], old_data_mouse2.ranges[i], old_data_mouse3.ranges[i],
+                 old_data_mouse4.ranges[i], data.ranges[i]])
+            print(variance_between_points)
+
+            # if the variance exists and is greater than a threshold, add the point to another array
+            # this determines if the point is part of a moving object or not
+            if not math.isnan(variance_between_points) and variance_between_points >= change_in_time:
                 points_to_be_converted.append(data.ranges[i])
             else:
+
+                # append inf to make sure the point cloud does not display this angle
                 points_to_be_converted.append(numpy.inf)
     else:
         print("all new")
+
+        # add old data to the arrays as a placeholder
+        old_data_mouse1 = data
+        old_data_mouse2 = data
+        old_data_mouse3 = data
+        old_data_mouse4 = data
         points_to_be_converted = data.ranges
 
-    old_data_mouse = data
+    # slide the histories of the laser scan one more array down (keeps the 5 most recent laser scans)
+    old_data_mouse4 = old_data_mouse3
+    old_data_mouse3 = old_data_mouse2
+    old_data_mouse2 = old_data_mouse1
+    old_data_mouse1 = data
+
+    # init another laser scan just for the points that are tracked as movement
     change_mouse = LaserScan()
     change_mouse = copy.deepcopy(data)
     change_mouse.ranges = points_to_be_converted
+    # print(len(points_to_be_converted))
+
+    # make point clouds for the movement
     pc_li_mouse = make_PC_from_Laser_display(change_mouse)
+
+    # make point clouds for the whole laser scan
     pc_li_mouse_all = make_PC_from_Laser_display(data)
 
 
 def callback_snake_laser(data):
-    global pc_li_snake, old_data_snake, pc_li_snake_all
+    global pc_li_snake, old_data_snake1, old_data_snake2, old_data_snake3, old_data_snake4, pc_li_snake_all
 
+    # have 5 arrays store 5 most recent laser scans
+    # find variance of each index in 5 frames
+    # if variance is greater than threshold, then detect as movmenet, else nah
     points_to_be_converted = []
     print("snake")
 
-    if old_data_snake:
-        data.ranges = list(data.ranges)
-        for i in range(0, len(data.ranges)):
+    # if there is history of the laser scans (should work besides the very first laser scan)
+    if old_data_snake1 and old_data_snake2 and old_data_snake3 and old_data_snake4:
 
-            if abs(data.ranges[i] - old_data_snake.ranges[i]) >= change_in_time:
+        # convert to lists
+        data.ranges = list(data.ranges)
+        old_data_snake1.ranges = list(old_data_snake1.ranges)
+        old_data_snake2.ranges = list(old_data_snake2.ranges)
+        old_data_snake3.ranges = list(old_data_snake3.ranges)
+        old_data_snake4.ranges = list(old_data_snake4.ranges)
+
+        # iterating through each angle of the laser
+        for i in range(0, len(data.ranges)):
+            # if abs(data.ranges[i] - old_data_hog1.ranges[i]) >= change_in_time:
+            #     points_to_be_converted.append(data.ranges[i])
+            # else:
+            #     points_to_be_converted.append(numpy.inf)
+
+            # calculate the variance of the points of the 5 frames at each angle
+            variance_between_points = numpy.var(
+                [old_data_snake1.ranges[i], old_data_snake2.ranges[i], old_data_snake3.ranges[i],
+                 old_data_snake4.ranges[i], data.ranges[i]])
+            print(variance_between_points)
+
+            # if the variance exists and is greater than a threshold, add the point to another array
+            # this determines if the point is part of a moving object or not
+            if not math.isnan(variance_between_points) and variance_between_points >= change_in_time:
                 points_to_be_converted.append(data.ranges[i])
             else:
+
+                # append inf to make sure the point cloud does not display this angle
                 points_to_be_converted.append(numpy.inf)
     else:
         print("all new")
+
+        # add old data to the arrays as a placeholder
+        old_data_snake1 = data
+        old_data_snake2 = data
+        old_data_snake3 = data
+        old_data_snake4 = data
         points_to_be_converted = data.ranges
 
-    old_data_snake = data
+    # slide the histories of the laser scan one more array down (keeps the 5 most recent laser scans)
+    old_data_snake4 = old_data_snake3
+    old_data_snake3 = old_data_snake2
+    old_data_snake2 = old_data_snake1
+    old_data_snake1 = data
+
+    # init another laser scan just for the points that are tracked as movement
     change_snake = LaserScan()
     change_snake = copy.deepcopy(data)
     change_snake.ranges = points_to_be_converted
+    # print(len(points_to_be_converted))
+
+    # make point clouds for the movement
     pc_li_snake = make_PC_from_Laser_display(change_snake)
+
+    # make point clouds for the whole laser scan
     pc_li_snake_all = make_PC_from_Laser_display(data)
 
 
